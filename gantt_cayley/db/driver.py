cf4a6117d4e98abe4ba986b73d1dcddec4de27b3
reconrus <reconrus@yandex.ru>
2019-04-21 14:48:22 +0300

    universal parser

--- gantt_cayley/db/driver.py
+++ gantt_cayley/db/driver.py
@@ -3,6 +3,13 @@ import requests
 from models import *
 
 class DatabaseDriver():
+    
+    types = {
+            "USER": User,
+            "GROUP": Group,
+            "PROJECT": Project,
+            "TASK": Task
+        }
 
     def __init__(self, address=""):
 
@@ -10,43 +17,33 @@ class DatabaseDriver():
         self.g = GraphObject()
 
     def get_user_by_id(self, id):
-        # query = self.g.V("user:"+str(id)).Out().All()
-        query = self.g.V("user:"+str(id)).Out(["username", "password", "email"], "pred").All()
+        query = self.g.V("user:"+str(id)).Out(["username", "password", "email", "in_group"], "pred").All()
         response = self.client.Send(query)
         return response.result["result"]
 
-    def _parse_user(self, response):
-
-        created_user = []
-        users = []
+    def _parse_object_response(self, response, label):
+        created_objects = []
+        objects = []
         for i in response:
-            if not i['user_id'] in created_user:
-                users.append(User(i['user_id']))
-                created_user.append(i['user_id'])
-            user = next((x for x in users if x.id == i['user_id']), None)
-            setattr(user, i['pred'], i['id'])
+            if not i['source_id'] in created_objects:
+                objects.append(self.types[label](i['source_id']))
+                created_objects.append(i['source_id'])
+            user = next((x for x in objects if x.id == i['source_id']), None)
+            if type(getattr(user, i['pred'])) == type(set()):
+                getattr(user, i['pred']).add(i['id'])
+            else: 
+                setattr(user, i['pred'], i['id'])
 
-        return users
+        return objects
 
-    def _filter_by_label(self, label, value=None):
+    def _filter_by_label(self, label):
 
-        if label == "USER": 
-            query = "g.V().LabelContext(\"%s\").In().Tag(\"user_id\").LabelContext(null) \
-                .Out([\"username\", \"password\", \"email\", \"in_group\"], \"pred\").All()" % (label)        
-            response = self.client.Send(query).result["result"] 
+        query = "g.V().LabelContext(\"%s\").In().Tag(\"source_id\").LabelContext(null) \
+            .Out([], \"pred\").All()" % (label)  
 
-            return self._parse_user(response)
-
-        if label == "GROUP":
-            query = self.g.V().Out(["name", "project"], "pred").All()
-
-        if label == "PROJECT":
-            query = self.g.V().Out("name", "pred").All()
-
-        # query = "g.V().LabelContext(\"%s\").In().LabelContext(null).Out().All()" % (label)
         response = self.client.Send(query).result["result"] 
-        # return set((i['id'] for i in response))
-        return response
+
+        return self._parse_object_response(response, label)
 
     def _filter_by_parameter(self, parameter, value=None):
         if value is None:
@@ -60,16 +57,13 @@ class DatabaseDriver():
 
     def filter_by(self, node_type, value=None):
 
-        query = "g.V().Labels().All()"
-        labels = self.client.Send(query).result["result"]
         upper_node_type = node_type.upper()
 
-        if {'id': upper_node_type} in labels:
-            result = self._filter_by_label(upper_node_type, value)
+        if upper_node_type in self.types:
+            result = self._filter_by_label(upper_node_type)
             
         else:
             result = self._filter_by_parameter(node_type, value)
         
         return None or result
-
 
--- gantt_cayley/db/models.py
+++ gantt_cayley/db/models.py
@@ -1,46 +1,46 @@
 class User: 
     
-    def __init__(self, user_id, username=None, password=None, email=None, group = None):
+    def __init__(self, user_id, username=None, password=None, email=None, group=None):
         self.id = user_id
         self.username = username 
         self.password = password
         self.email = email
-        self.in_group = group
+        self.in_group = set() if group == None else group
 
     def add_to_group(self, group):
         self.in_group = group
 
 class Group: 
     
-    def __init__(self, group_id, name=None, projects = []):
+    def __init__(self, group_id, name=None, projects=None):
         self.id = group_id
         self.name = name
-        self.projects = projects
+        self.projects = set() if projects == None else projects
 
     def add_project(self, project):
         self.projects.append(project)
 
 class Project: 
 
-    def __init__(self, project_id, name=None, description=None, chart_link=None, tasks = []):
+    def __init__(self, project_id, name=None, description=None, chart_link=None, tasks=None):
         self.id = project_id
         self.name = name
-        self.tasks = tasks
         self.description = description
         self.chart_link = chart_link
+        self.tasks = set() if tasks == None else tasks
 
     def add_task(self, task):
         self.tasks.append(task)
 
-class Tasks:
+class Task:
     
-    def __init__(self, task_id, title=None, description=None, start_date=None, end_date=None, assignees = []):
+    def __init__(self, task_id, title=None, description=None, start_date=None, end_date=None, assignees=None):
         self.id = task_id
         self.title = title
         self.description = description
         self.start_date = start_date
         self.end_date = end_date
-        self.assignees = assignees
+        self.assignees = set() if assignees == None else assignees
 
     def add_assignee(self, assignee):
         self.assignees.append(assignee)
