d49d7350c066dd190bfe617a8cb91bd1315bcd9d
fenchelfen <lyamets.misha@gmail.com>
2019-04-23 23:12:08 +0300

    Update chart_builder

--- chart_builder.py
+++ chart_builder.py
@@ -2,35 +2,35 @@ from requests.auth import HTTPBasicAuth
 import plotly.figure_factory as ff
 import plotly.tools as tools
 import plotly.plotly as py
-from random import randint
+from random import randint, uniform
 import requests
 
-username = 'fenchelfen'
-api_key = 'A813566osbTCR6NQ923L'
+username = 'shittyinstagramvanhalen'
+api_key = '4HaREPLKtBw2m0hLYDKh'
 
 tools.set_credentials_file(username=username, api_key=api_key)
-
-df = [
-    dict(Task='Morning Sleep', Start='2016-01-01', Finish='2016-01-01 6:00:00', Resource='Sleep'),
-    dict(Task='Breakfast', Start='2016-01-01 7:00:00', Finish='2016-01-01 7:30:00', Resource='Food'),
-    dict(Task='Work', Start='2016-01-01 9:00:00', Finish='2016-01-01 11:25:00', Resource='Brain'),
-    dict(Task='Break', Start='2016-01-01 11:30:00', Finish='2016-01-01 12:00:00', Resource='Rest'),
-    dict(Task='Lunch', Start='2016-01-01 12:00:00', Finish='2016-01-01 13:00:00', Resource='Food'),
-    dict(Task='Work', Start='2016-01-01 13:00:00', Finish='2016-01-01 17:00:00', Resource='Brain'),
-    dict(Task='Exercise', Start='2016-01-01 17:30:00', Finish='2016-01-01 18:30:00', Resource='Cardio'),
-    dict(Task='Post Workout Rest', Start='2016-01-01 18:30:00', Finish='2016-01-01 19:00:00', Resource='Rest'),
-    dict(Task='Dinner', Start='2016-01-01 19:00:00', Finish='2016-01-01 20:00:00', Resource='Food'),
-    dict(Task='Evening Sleep', Start='2016-01-01 21:00:00', Finish='2016-01-01 23:59:00', Resource='Sleep')
-]
 
 
 def create_chart(df, title):
     r = lambda: randint(0, 255)
     colors = ['#%02X%02X%02X' % (r(), r(), r()) for _ in range(len(df))]
-    print(colors)
+    bar_width = uniform(0.4, 1) if len(df) < 5 else uniform(0.1, 0.3)
     fig = ff.create_gantt(df, colors=colors, title=title,
-                          show_colorbar=True, bar_width=0.8, showgrid_x=True, showgrid_y=True)
-    return py.plot(fig, filename='gantt-hours-minutes', world_readable=True)
+                          show_colorbar=True, bar_width=bar_width, showgrid_x=True, showgrid_y=True)
+    return py.plot(fig, filename=title, world_readable=True, auto_open=False)
+
+
+def define_data(tasks):
+    df = []
+    for task in tasks:
+        df.append(
+            dict(
+                Task=task.title,
+                Start=task.start_date,
+                Finish=task.end_date
+            )
+        )
+    return df
 
 
 def delete_chart(chart_link):
